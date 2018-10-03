ee913d2b4a09e048b003b660500263dbb96d2704
Xavier-Yin <bug313@gmail.com>
2018-10-04 00:09:36 +0800

    load from csv, write to temp.csv Temperature now assumes Celsius instead of Kelvin as sample csv

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import inspect
 from functools import wraps
+import csv
+
+SAMPLE_CSV = "d11B-pH_Error_estimation.csv"
 
 def showparam(f):
     @wraps(f)
@@ -13,8 +16,13 @@ def showparam(f):
         return f(*args, **kwds)
     return wrapper
 
-
+ALPHA = 1.0272
 N_SAMPLE = 1000000
+FIELDNAMES = ['Sample ID', 'Salinity', 'SD of Salinity', 'Temperature', 'SD of Temperature', 'pKb', 'SD of pKb', 'd11Bsw', 'SD of d11Bsw', 'd11Bc', 'SD of d11Bc', 'pHcf', 'SD of pHcf']
+OUTPUT_FIELDS = ['pKb', 'SD of pKb', 'pHcf', 'SD of pHcf']
+MENDATORY_FIELDS = ['Salinity', 'SD of Salinity', 'Temperature', 'SD of Temperature', 'd11Bsw', 'SD of d11Bsw', 'd11Bc']
+CALC_FIELDS = MENDATORY_FIELDS + ['SD of d11Bc']
+
 
 def print_stats(var, var_name="unnamed_variable"):
     print("{v:12}: avg{m:12}, stdev={s:12}".format(v=var_name, m=var.mean(), s=var.std()))
@@ -45,7 +53,7 @@ def demo():
     """
     mu, sigma = 0, 1
     print("N={n}, mu={m}, sigma={s}".format(n=N_SAMPLE, m=mu, s=sigma))
-    x = np.random.normal(mu, sigma, N_SAMPLE)   # N-sized array of random variable x
+    x = sampling(mu, sigma, N_SAMPLE)   # N-sized array of random variable x
     y = x**2 + 2*x + 1                          # N-sized array of deduced variable y
     present_vars((x, y), ("x", "y"))
 
@@ -55,7 +63,7 @@ def pH_formula(pKb, d11Bsw, d11Bcarbonate, alpha):
 @showparam
 def calc_pH_by_d11Bcarbonate(mu_d11Bcarbonate, sigma_d11Bcarbonate, N=N_SAMPLE):
     # random variable
-    d11Bcarbonate = np.random.normal(mu_d11Bcarbonate, sigma_d11Bcarbonate, N)
+    d11Bcarbonate = sampling(mu_d11Bcarbonate, sigma_d11Bcarbonate, N)
     # constants
     pKb = 8.6152
     d11Bsw = 39.61
@@ -66,27 +74,69 @@ def calc_pH_by_d11Bcarbonate(mu_d11Bcarbonate, sigma_d11Bcarbonate, N=N_SAMPLE):
     present_vars((d11Bcarbonate, pH), ("d11Bcarbonate", "pH"))
 
 def gen_pKb_by_S_and_T(mu_pKb_S, sigma_pKb_S, mu_pKb_T, sigma_pKb_T, N=N_SAMPLE):
-    S = np.random.normal(mu_pKb_S, sigma_pKb_S, N)
-    T = np.random.normal(mu_pKb_T, sigma_pKb_T, N)
+    S = sampling(mu_pKb_S, sigma_pKb_S, N)
+    T = sampling(mu_pKb_T+273.15, sigma_pKb_T, N)
 #     k1, k2, k3, k4, k5, k6 = 8966.9, 2890.53, 77.942, 1.728, 0.0996, 148.0248, 137.1942, 1.62142, 24.4344, 25.085, 0.2474, 0.053105
     return -np.log10(np.exp((-8966.9-2890.53*(S**0.5)-77.942*S+1.728*(S**1.5)-0.0996*(S**2))/T+148.0248+137.1942*(S**0.5)+1.62142*S-(24.4344+25.085*(S**0.5)+0.2474*S)*np.log(T)+0.053105*(S**0.5)*T))
 
-@showparam
-def calc_pH_by_d11Bcarbonate_with_random_parameters(mu_d11Bcarbonate, sigma_d11Bcarbonate, mu_pKb_S, sigma_pKb_S, mu_pKb_T, sigma_pKb_T, mu_d11Bsw, sigma_d11Bsw, N=N_SAMPLE):
-    
-    # random variable
-    d11Bcarbonate = np.random.normal(mu_d11Bcarbonate, sigma_d11Bcarbonate, N)
-    pKb = gen_pKb_by_S_and_T(mu_pKb_S, sigma_pKb_S, mu_pKb_T, sigma_pKb_T, N)
-    d11Bsw = np.random.normal(mu_d11Bsw, sigma_d11Bsw, N)
-#     present_vars((d11Bcarbonate, pKb, d11Bsw), ("d11Bcarbonate", "pKb", "d11Bsw"))
-    # constants
+def sampling(mu, std, N):
+    if std < 0:
+        raise ValueError
+    elif std == 0:
+        return np.array([mu]*N)
+    else:
+        return np.random.normal(mu, std, N)
+
+def gen_valid_sample(s, s_std, t, t_std, sw, sw_std, c, c_std, N=N_SAMPLE):
+    d11Bcarbonate = sampling(c, c_std, N)
+    pKb = gen_pKb_by_S_and_T(s, s_std, t, t_std, N)
+    d11Bsw = sampling(sw, sw_std, N)
     alpha = 1.0272
-    # formula
     pH = pH_formula(pKb, d11Bsw, d11Bcarbonate, alpha)
     pH_valid_mask = ~np.isnan(pH)
-    print("valid sample count: {v} ({p}%)".format(v=len(pH[pH_valid_mask]), p=100.0*len(pH[pH_valid_mask])/N))
-    present_vars((d11Bcarbonate[pH_valid_mask], pKb[pH_valid_mask], d11Bsw[pH_valid_mask], pH[pH_valid_mask]), ("d11Bcarbonate", "pKb", "d11Bsw", "pH"))
-        
+    valid_ratio = 1.0*len(pH[pH_valid_mask])/N
+    return d11Bcarbonate[pH_valid_mask], pKb[pH_valid_mask], d11Bsw[pH_valid_mask], pH[pH_valid_mask], valid_ratio
+
+@showparam
+def calc_pH_by_d11Bcarbonate_with_random_parameters(mu_d11Bcarbonate, sigma_d11Bcarbonate, mu_pKb_S, sigma_pKb_S, mu_pKb_T, sigma_pKb_T, mu_d11Bsw, sigma_d11Bsw, N=N_SAMPLE):
+    d11Bcarbonate_valid, pKb_valid, d11Bsw_valid, pH_valid, valid_ratio = gen_valid_sample(mu_pKb_S, sigma_pKb_S, mu_pKb_T, sigma_pKb_T, mu_d11Bsw, sigma_d11Bsw, mu_d11Bcarbonate, sigma_d11Bcarbonate, N)
+    print("valid sample count: {v} ({p}%)".format(v=len(pH_valid), p=100.0*valid_ratio))
+    present_vars((d11Bcarbonate_valid, pKb_valid, d11Bsw_valid, pH_valid), ("d11Bcarbonate", "pKb", "d11Bsw", "pH"))
+      
+
+
+DEFAULT_SD_OF_D11BC = 0.82
+      
+def test_csv(filename):
+    print(filename)
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile)
+        if reader.fieldnames != FIELDNAMES:
+            print("error: illegal fieldnames: {n}".format(n=reader.fieldnames))
+        data = [row for row in reader]
+    with open("temp.csv", 'wb') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
+        writer.writeheader()
+        for row in data:
+#             print row
+            if not all([row[k] for k in OUTPUT_FIELDS]):
+                try:
+                    assert all([row[k] for k in MENDATORY_FIELDS])
+                    if not row['SD of d11Bc']:
+                        row['SD of d11Bc'] = DEFAULT_SD_OF_D11BC
+                    for field in CALC_FIELDS:
+    #                     print field, row[field]
+                        row[field] = float(row[field])
+                    d11Bcarbonate_valid, pKb_valid, d11Bsw_valid, pH_valid, valid_ratio = gen_valid_sample(*[row[k] for k in CALC_FIELDS])
+                    row['pKb'] = pKb_valid.mean()
+                    row['SD of pKb'] = pKb_valid.std()
+                    row['pHcf'] = pH_valid.mean()
+                    row['SD of pHcf'] = pH_valid.std()
+                    print("Sample ID {s}: {o}, ratio={r}%".format(s=row["Sample ID"], o=[row[k] for k in OUTPUT_FIELDS], r=100.0*valid_ratio))
+                except Exception as e:
+                    print(e)
+                    print("warning: skipped Sample ID {s}".format(s=row["Sample ID"]))
+            writer.writerow(row)
 
 if __name__ == "__main__":
     # Run demo
@@ -95,4 +145,5 @@ if __name__ == "__main__":
     
 #     calc_pH_by_d11Bcarbonate(17.39, 0.19)
 
-    calc_pH_by_d11Bcarbonate_with_random_parameters(12.35, 0.66/2, 32, 0.2, 298.15, 0.1, 39.61, 0.4/2)
+#     calc_pH_by_d11Bcarbonate_with_random_parameters(12.35, 0.66/2, 32, 0.2, 25, 0.1, 39.61, 0.4/2)
+    test_csv(SAMPLE_CSV)
